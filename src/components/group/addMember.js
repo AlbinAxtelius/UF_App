e4d4779f166e7c1369b3d54cfd583c939a4535e9
unknown <albin@axtelius.se>
2019-01-28 15:27:43 +0100

    Loading icon for UX

--- src/components/group/addMember.js
+++ src/components/group/addMember.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
   Text,
   View,
@@ -6,12 +6,13 @@ import {
   Modal,
   TextInput,
   StyleSheet,
-  TouchableNativeFeedback
-} from 'react-native'
+  TouchableNativeFeedback,
+  ActivityIndicator
+} from "react-native";
 
-import { Ionicons } from '@expo/vector-icons';
-import fire from '../../config/config';
-import globalstyles from '../../styles/globalstyle'
+import { Ionicons } from "@expo/vector-icons";
+import fire from "../../config/config";
+import globalstyles from "../../styles/globalstyle";
 
 export class AddMember extends Component {
   constructor(props) {
@@ -21,23 +22,25 @@ export class AddMember extends Component {
       email: "",
       message: "",
       loading: false
-    }
+    };
   }
 
   handleAccept = async () => {
-    this.setState({ loading: true, message: "" })
-    fetch(`https://us-central1-f-app-273d0.cloudfunctions.net/addUser?user=${this.state.email}&group=${this.props.groupId}&groupname=${this.props.groupName}`)
+    this.setState({ loading: true, message: "" });
+    fetch(
+      `https://us-central1-f-app-273d0.cloudfunctions.net/addUser?user=${
+        this.state.email
+      }&group=${this.props.groupId}&groupname=${this.props.groupName}`
+    )
       .then(res => res.json())
       .then(result => {
-        console.log(result);
+        this.setState({loading: false})
         if (result.message.message)
           this.setState({ message: result.message.message });
-        else
-          this.setState({ message: result.message });
-        if (result.code === 200)
-          this.props.handleClose();
-      })
-  }
+        else this.setState({ message: result.message });
+        if (result.code === 200) this.props.handleClose();
+      });
+  };
   render() {
     return (
       <Modal
@@ -47,29 +50,35 @@ export class AddMember extends Component {
       >
         <View style={globalstyles.popupContainer}>
           <View style={globalstyles.popupHeader}>
-              <TouchableNativeFeedback
+            <TouchableNativeFeedback
               onPress={() => this.props.handleClose()}
-              background={TouchableNativeFeedback.SelectableBackground()}>
-                <View style={globalstyles.popupBack}>
-                  <Ionicons name="md-arrow-back" size={24} />
-                </View>
-              </TouchableNativeFeedback>
-              <Text style={globalstyles.greenHeaderText}>Lägg till medlem</Text>
-            </View>
+              background={TouchableNativeFeedback.SelectableBackground()}
+            >
+              <View style={globalstyles.popupBack}>
+                <Ionicons name="md-arrow-back" size={24} />
+              </View>
+            </TouchableNativeFeedback>
+            <Text style={globalstyles.greenHeaderText}>Lägg till medlem</Text>
+          </View>
           <TextInput
             underlineColorAndroid="#27AE60"
             value={this.state.email}
             onChangeText={email => this.setState({ email })}
             placeholder="Email"
-            style={globalstyles.inputBig} />
-          <View style={{ alignSelf: 'flex-start', marginLeft: "2.5%" }}>
+            style={globalstyles.inputBig}
+          />
+          <View style={{ alignSelf: "flex-start", marginLeft: "2.5%" }}>
             <Text>{this.state.message}</Text>
           </View>
-          <Button onPress={() => this.handleAccept()} title="Lägg till" />
-        </View >
+          {this.state.loading ? (
+            <ActivityIndicator />
+          ) : (
+            <Button onPress={() => this.handleAccept()} title="Lägg till" />
+          )}
+        </View>
       </Modal>
-    )
+    );
   }
 }
 
-export default AddMember
+export default AddMember;
